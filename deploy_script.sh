adc2aabfd5a37a705e77afdae4c7c504225e9199
RobLewisQA <rlewis@academytrainee.com>
2021-04-09 13:47:07 +0000

    jenkinsfile update

--- deploy_script.sh
+++ deploy_script.sh
@@ -4,5 +4,5 @@ scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@manager-main:/home/jenkins/dock
 ssh -i ~/.ssh/id_rsa jenkins@manager-main << EOF
     export DATABASE_URI=${DATABASE_URI}
     export SECRET_KEY=${SECRET_KEY}
-    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml
+    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml prize_generator
 EOF
