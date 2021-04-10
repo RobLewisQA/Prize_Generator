7fcaaec0551b61f3a0a16668dc450da471180067
RobLewisQA <rlewis@academytrainee.com>
2021-04-10 19:36:40 +0000

    rectified import error 'db'

--- frontend/Tests/test_file.py
+++ frontend/Tests/test_file.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 from flask import url_for
 from flask_testing import TestCase
 from flask_sqlalchemy import SQLAlchemy
-from application import app, db
+from application import app
 from flask import Flask, redirect, request, url_for,render_template
 
 
