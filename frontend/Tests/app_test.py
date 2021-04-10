f6f37fb990f7df0ddf3bf97225a8d6458d8ac28c
RobLewisQA <rlewis@academytrainee.com>
2021-04-10 09:57:23 +0000

    removed unnecessary create.py

--- frontend/Tests/app_test.py
+++ frontend/Tests/app_test.py
@@ -7,5 +7,14 @@ from application import app, db
 from flask import Flask, redirect, request, url_for,render_template
 
 
+class TestBase(TestCase):
+    def create_app(self):
+        return app
 
+# class TestResponse(TestBase):
 
+#     def test_num_generator(self):
+#         response = self.client.get('http://frontend:5003/')
+#         assert response.status_code == 200
+#         output = response.data 
+#         assertIn() 
