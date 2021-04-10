b9189165c88cb0189e83f59377622d7892cf2eb9
RobLewisQA <rlewis@academytrainee.com>
2021-04-10 18:43:40 +0000

    added test for config - backend and number and letter and front end

--- back-end/Tests/test_file.py
+++ back-end/Tests/test_file.py
@@ -40,6 +40,18 @@ class TestViews(TestBase):
             self.assertIn(b'lose', response.data)
             self.assertIn(b'no prize', response.data)
     
+    def test_frontend_integration(self):
+        with requests_mock.mock() as m:
+            m.get("http://random_numbers:5001/rnum", text = "500")
+            m.get("http://random_letters:5002/rletters", text = "a")
+            response = self.client.get(url_for('prizegen'))
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'lose', response.data)
+
+            response = self.client.get("http://frontend:5003")
+            self.assertIn(b'no prize', response.data)
+
+    
 
         
 
