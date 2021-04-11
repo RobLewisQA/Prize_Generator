4d6ce8c167788c69d94e854ebbef0145c1b0d852
RobLewisQA <rlewis@academytrainee.com>
2021-04-11 19:19:01 +0000

    frontend/application complete

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -1,19 +1,20 @@
 from flask import Flask, redirect, request, url_for,render_template, Response, jsonify
-from application import app#, db, models
+from application import app
 from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 import requests
 
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])    # returns the homepage
 def home():
     return render_template('home.html')
 
 
-@app.route("/prize-board", methods=['GET'])
+@app.route("/prize-board", methods=['GET'])    # returns the pages for win or lose
 def frontend():
+
     data=requests.get("http://back-end:5000/prizegen").json()
-    host_name = request.host
+    
     if data["win_lose"] == 'win':
         return render_template('winner.html', data=data)
     else:
