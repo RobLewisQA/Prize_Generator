fc0a891a2066615175ad0a9a8acebc773bbed3e3
RobLewisQA <rlewis@academytrainee.com>
2021-04-11 21:36:36 +0000

    update to numbers test

--- random_numbers/Tests/app_test.py
+++ random_numbers/Tests/app_test.py
@@ -10,13 +10,13 @@ class TestBase(TestCase):
 
 class TestResponse(TestBase):
 
-    def test_num_generator(self):
+    def test_num_generator(self):    # testing that the output of the function is within the parameters
         response = self.client.get('http://random_numbers:5001/rnum')
         assert response.status_code == 200
         output = response.data 
         assert int(output) > 99 & int(output)<1001
 
-    def test_rand_numbers(self):
+    def test_rand_numbers(self):    # testing the response from the back-end when 500 is the output from random_numbers
         with patch('requests.get') as g:
             g.return_value.text = "500"
             response = self.client.get('http://back-end:5000/prizegen')
