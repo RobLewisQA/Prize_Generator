39a8a7ea005c3fedb996f95c3cd406dbb5bed8c5
RobLewisQA <rlewis@academytrainee.com>
2021-04-11 19:39:06 +0000

    added comment posting to random letters

--- random_numbers/application/routes.py
+++ random_numbers/application/routes.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, request, url_for,render_template
 from application import app
 import random
 
-@app.route('/rnum', methods=['GET'])
+@app.route('/rnum', methods=['GET'])    # generating a random 3 digit integer
 def rnum_generator():
     num = random.randint(100,999)
     return f'{num}'
