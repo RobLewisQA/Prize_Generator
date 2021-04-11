9c89f09750cce8b813fd225fbd64fbd14fc26de5
RobLewisQA <rlewis@academytrainee.com>
2021-04-11 23:51:27 +0000

    changed service 1 and service 4

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -10,11 +10,18 @@ def home():
     return render_template('home.html')
 
 
-@app.route("/prize-board", methods=['GET'])    # returns the pages for win or lose
+@app.route("/prize-board", methods=['GET'])    #
 def frontend():
+    random_number = requests.get("http://random_numbers:5001/rnum").text
+    random_letter = requests.get("http://random_letters:5002/rletters").text
 
-    data=requests.get("http://back-end:5000/prizegen").json()
+    data = requests.get("http://back-end:5000/prizegen").json()
     
+    new_entry = Outcomes(rand_number=data["rand_number"],win_lose=data["win_lose"],prize=data["prize"])    # add new record to database
+    db.session.add(new_entry)
+    db.session.commit()
+    
+
     if data["win_lose"] == 'win':
         return render_template('winner.html', data=data)
     else:
