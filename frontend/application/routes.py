1972143180baa59cc322c53a559a1ec03f683135
RobLewisQA <rlewis@academytrainee.com>
2021-04-12 07:16:45 +0000

    adding a decode statement for json

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -13,7 +13,7 @@ def home():
 
 @app.route("/prize-board", methods=['GET'])
 def frontend():
-    data=requests.get("http://back-end:5000/prizegen").json()
+    data=requests.get("http://back-end:5000/prizegen").json()#.response.data.decode('utf-8')#
     host_name = request.host
     if data["win_lose"] == 'win':
         return render_template('winner.html', data=data)
