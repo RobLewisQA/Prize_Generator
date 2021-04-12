227416ad4a84d68dec45530be45639f0869e3619
RobLewisQA <rlewis@academytrainee.com>
2021-04-12 06:44:45 +0000

    trying to rectify code issue

--- frontend/application/__init__.py
+++ frontend/application/__init__.py
@@ -3,4 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@mysql:3306/flask-db'
+
+#db = SQLAlchemy(app)
+
 from application import routes
--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -1,22 +1,29 @@
 from flask import Flask, redirect, request, url_for,render_template, Response, jsonify
-from application import app
+from application import app#, db, models
 from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 import requests
 import json
 
 
-@app.route('/', methods=['GET','POST'])    # returns the homepage
+@app.route('/', methods=['GET','POST'])
 def home():
     return render_template('home.html')
 
 
+<<<<<<< HEAD
 @app.route("/prize-board", methods=['GET'])    # returns the pages for win or lose.
 def frontend():
 
     data = requests.get("http://back-end:5000/prizegenerator").json()
     #data =  json.loads(response)
 
+=======
+@app.route("/prize-board", methods=['GET'])
+def frontend():
+    data=requests.get("http://back-end:5000/prizegen").json()
+    host_name = request.host
+>>>>>>> parent of 4d6ce8c... frontend/application complete
     if data["win_lose"] == 'win':
         return render_template('winner.html', data=data)
     else:
