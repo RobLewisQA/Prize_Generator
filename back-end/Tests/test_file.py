c55fa2454040411bddbeba70ef1bea84f57897a9
RobLewisQA <rlewis@academytrainee.com>
2021-04-12 09:00:05 +0000

    commit

--- back-end/Tests/test_file.py
+++ back-end/Tests/test_file.py
@@ -28,43 +28,36 @@ class TestBase(TestCase):    # create a replacement version for testing database
 class TestBackend(TestBase):    # testing submission to the database
     def test_backend_engine(self):
         response = Outcomes.query.all()
-        #self.assertEqual(response.status_code, 200) 
-
-    
-    def test_backend_lose(self):    # testing the backend for output
-        
-        with requests_mock.mock() as m:    
-            response = self.client.get("http://back-end:5000/prizegen")
-            #assert ('win' in response.data == True) or ('lose' in response.data == True)
+        assert type(response) == list
     
     def test_backend_goldwin(self):    # testing the backend output given a gold-winning output from the two middle services
         with requests_mock.mock() as m:
             m.get("http://random_numbers:5001/rnum", text = "201")
             m.get("http://random_letters:5002/rletters", text = "e")
-            response = self.client.get("http://back-end:5000/prizegenerator")
+            response = self.client.get("http://back-end:5000/prizegen")
             self.assertIn(b'win', response.data)
             self.assertIn(b'Gold', response.data)
     
     def test_backend_silverwin(self):    # testing the backend output given a silver-winning output from the two middle services
         with requests_mock.mock() as m:
-            m.get('http://random_numbers:5001/rnum', text = "207")
-            m.get('http://random_letters:5002/rletters', text = "a")
-            response = self.client.get('http://back-end:5000/prizegenerator')
+            m.get("http://random_numbers:5001/rnum", text = "207")
+            m.get("http://random_letters:5002/rletters", text = "a")
+            response = self.client.get("http://back-end:5000/prizegen")
             self.assertIn(b'win', response.data)
             self.assertIn(b'Silver', response.data)
 
     def test_backend_bronzerwin(self):    # testing the backend output given a bronze-winning output from the two middle services
         with requests_mock.mock() as m:
-            m.get('http://random_numbers:5001/rnum', text = "131")
-            m.get('http://random_letters:5002/rletters', text = "c")
-            response = self.client.get('http://back-end:5000/prizegenerator')
+            m.get("http://random_numbers:5001/rnum", text = "131")
+            m.get("http://random_letters:5002/rletters", text = "c")
+            response = self.client.get("http://back-end:5000/prizegen")
             self.assertIn(b'win', response.data)
             self.assertIn(b'Bronze', response.data)
     
-    def test_frontend_integration(self):    # testing the backend output given a losing output from the two middle services
+    def test_backend_lose(self):    # testing the backend output given a losing output from the two middle services
         with requests_mock.mock() as m:
-            m.get('http://random_numbers:5001/rnum', text = "500")
-            m.get('http://random_letters:5002/rletters', text = "a")
-            response = self.client.get('http://back-end:5000/prizegenerator')
+            m.get("http://random_numbers:5001/rnum", text = "500")
+            m.get("http://random_letters:5002/rletters", text = "a")
+            response = self.client.get("http://back-end:5000/prizegen")
             self.assertEqual(response.status_code, 200)
             self.assertIn(b'lose', response.data)
