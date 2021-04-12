80c14800d8002c1c84fc1fc98aea1141aee3552e
RobLewisQA <rlewis@academytrainee.com>
2021-04-12 08:54:40 +0000

    removed models in frontend

--- frontend/application/models.py
+++
@@ -1,8 +0,0 @@
-from application import db 
-from flask_sqlalchemy import SQLAlchemy
-
-class Outcomes(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	rand_number = db.Column(db.String(150), nullable=False, unique=True)
-	win_lose = db.Column(db.String(150), nullable=False)
-	prize = db.Column(db.String(150), nullable=False)
